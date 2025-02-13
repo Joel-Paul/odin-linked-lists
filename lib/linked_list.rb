3fee5aa76892d8f557d79e7138b3ad6975d2da56
Joel Paul <joel.paul@student.unsw.edu.au>
2025-02-14 10:48:18 +1100

    Implement append

---
+++ lib/linked_list.rb
@@ -0,0 +1,27 @@
+
+class LinkedList
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    node = Node.new value
+    if @tail.nil?
+      @head = node
+      @tail = node
+    else
+      @tail.next_node = node
+      @tail = node
+    end
+  end
+end
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value=nil, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
